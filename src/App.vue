254a3c3a601c1a72dce76f8f374b1b16af269760
Michal Korejwo <korejwo.michal@gmail.com>
2022-07-15 11:29:10 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link>
+    |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
+</template>
+
+<style>
+#app {
+  text-align: center;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <Main msg="Welcome to Your Vue.js + TypeScript Apps"/>
+  </div>
+</template>
+
+<script lang="ts">
+import {Options, Vue} from 'vue-class-component';
+// @ is an alias to /src
+import Main from '@/components/Main.vue';
+
+@Options({
+  components: {
+    Main,
+  },
+})
+export default class HomeView extends Vue {
+}
+</script>
